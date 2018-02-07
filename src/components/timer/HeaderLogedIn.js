49cf7290794771732d8b5089327a9ecc899eeed6
BakerShoeMaker <info@quackenworth.com>
2018-02-06 21:10:12 -0800

    Added all components except timer area.

--- src/components/timer/HeaderLogedIn.js
+++ src/components/timer/HeaderLogedIn.js
@@ -6,9 +6,28 @@ class HeaderLogedIn extends Component{
     render()
     {
         return(
-            <div className= "row  pt-3">
+            <div class = "row mr-4 mt-3 ">
+                <div class="col-2 ">
+                    <img src="assets/images/logo.png"/>
+                </div>
 
+                <div class="col-10 d-flex justify-content-end">
+                    <div class="dropdown">
+                        <a class="btn btn-secondary dropdown-toggle DropDownButton" href="#" role="button" id="dropdownMenuLink"
+                           data-toggle="dropdown" >
+                            <i class="fa fa-cog"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">My Profile</a>
+                            <a class="dropdown-item" href="#">Timer</a>
+                            <a class="dropdown-item" href="#">Log out</a>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="col-2"></div>
             </div>
+
         )
     }
 }
--- src/components/timer/Title.js
+++ src/components/timer/Title.js
@@ -6,9 +6,23 @@ class Title extends Component{
     render()
     {
         return(
-            <div className= "row  pt-3">
+           <div>
+               <div class="row d-flex justify-content-center timerAreaTextSetTime-todaysschedule pt-4">
+                   <h4> <i class="fa fa-clock-o mr-2 mt-4 pt-4 "></i>Today's Schedule</h4>
+               </div>
 
-            </div>
+               <div class="row ">
+                   <div class="col-3"></div>
+                   <div id="CurentTime" class="col-6 pt-2 d-flex justify-content-center timerAreaTextSetTime-current-time">
+
+                   </div>
+                   <div id="AddTimer" class="col-3 d-flex justify-content-start" >
+                       <button id="AddTimerButton" data-toggle="modal" data-target="#AddTimerModal"> +add timer</button>
+                   </div>
+               </div>
+               <hr/>
+
+           </div>
         )
     }
 }
